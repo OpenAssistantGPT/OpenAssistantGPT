5ed583d88c62ce27baaae89d1073a7d3e4af2cf5
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-11-21 08:52:38 -0500

    update

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,13 +2,11 @@ import { type NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GithubProvider from "next-auth/providers/github"
 
-import prisma from "@/lib/prisma";
 import { db } from "@/lib/db"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db as any),
   secret: process.env.NEXTAUTH_SECRET,
-  site: process.env.NEXTAUTH_URL,
   useSecureCookies: false,
   session: {
     strategy: "jwt",
@@ -18,42 +16,45 @@ export const authOptions: NextAuthOptions = {
   },
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID, 
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     })
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log("fire signin Callback");
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      console.log("fire redirect Callback");
-      return baseUrl;
-    },
-    async session({ session, user, token }) {
-      console.log("fire SESSION Callback");
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          id: token.id,
-          randomKey: token.randomKey,
-        },
-      };
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      console.log("fire jwt Callback");
-      if (user) {
-        const u = user as unknown as any;
-        return {
-          ...token,
-          id: u.id,
-          randomKey: u.randomKey,
-        };
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
       }
-      return token;
+
+      return session
     },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      console.log("dbUser", dbUser)
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+
   },
 };
 
--- middleware.ts
+++ middleware.ts
@@ -5,29 +5,9 @@ import { withAuth } from "next-auth/middleware"
 
 export default withAuth(
   async function middleware(req) {
-    const token = await getToken({ req })
-    const isAuth = !!token
-    const isAuthPage =
-      req.nextUrl.pathname.startsWith("/login")
 
-    if (isAuthPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
-      }
+    return NextResponse.next()
 
-      return null
-    }
-
-    if (!isAuth) {
-      let from = req.nextUrl.pathname;
-      if (req.nextUrl.search) {
-        from += req.nextUrl.search;
-      }
-
-      return NextResponse.redirect(
-        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-      );
-    }
   },
   {
     callbacks: {
