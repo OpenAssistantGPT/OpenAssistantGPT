6a2559cda50af4fb8e44a78615d0c8a17e5bc4e8
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-11-21 08:44:20 -0500

    update

--- config/marketing.ts
+++ config/marketing.ts
@@ -1,14 +1,10 @@
-import { MarketingConfig } from "types"
+import { MarketingConfig } from "@/types"
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
       title: "Features",
       href: "/#features",
-    },
-    {
-      title: "Pricing",
-      href: "/pricing",
     }
   ],
 }
