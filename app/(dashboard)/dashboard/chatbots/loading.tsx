0e86b84969912efa88194a813d56ac5632d18739
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-11-26 08:16:51 -0500

    update loading

--- app/(dashboard)/dashboard/chatbots/loading.tsx
+++ app/(dashboard)/dashboard/chatbots/loading.tsx
@@ -2,11 +2,11 @@ import { CardSkeleton } from "@/components/card-skeleton"
 import { DashboardHeader } from "@/components/header"
 import { DashboardShell } from "@/components/shell"
 
-export default function CrawlersLoading() {
+export default function ChatbotsLoading() {
     return (
         <DashboardShell>
             <DashboardHeader
-                heading="Dashboard"
+                heading="Chatbots"
                 text="Create and manage your chatbots."
             />
             <div className="grid gap-10">
