03e8f5e0f84a245c2f43a55d0a28101a35766241
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-11-25 19:22:23 -0500

    update

--- components/file-items.tsx
+++ components/file-items.tsx
@@ -1,5 +1,3 @@
-
-
 import Link from "next/link"
 import { CrawlerFile } from "@prisma/client"
 
@@ -7,11 +5,23 @@ import { formatDate } from "@/lib/utils"
 import { Skeleton } from "@/components/ui/skeleton"
 import { CrawlerFileOperations } from "@/components/file-operations"
 
+import { db } from "@/lib/db"
+
 interface CrawlFileProps {
     file: Pick<CrawlerFile, "id" | "name" | "blobUrl" | "createdAt" | "crawlerId">
 }
 
-export function CrawlerFileItem({ file }: CrawlFileProps) {
+export async function CrawlerFileItem({ file }: CrawlFileProps) {
+
+    const openAIFile = await db.openAIFile.findUnique({
+        select: {
+            id: true,
+        },
+        where: {
+            fileId: file.id
+        }
+    })
+
     return (
         <div className="flex items-center justify-between p-4">
             <div className="grid gap-1">
@@ -22,6 +32,17 @@ export function CrawlerFileItem({ file }: CrawlFileProps) {
                     {file.name}
                 </Link>
                 <div>
+                    {
+                        openAIFile ? (
+                            <p className="text-sm text-muted-foreground">
+                                <span className="text-green-600">Is published</span>
+                            </p>
+                        ) : (
+                            <p className="text-sm text-muted-foreground">
+                                <span className="text-red-600">Not published</span>
+                            </p>
+                        )
+                    }
                     <p className="text-sm text-muted-foreground">
                         {formatDate(file.createdAt?.toDateString())}
                     </p>
