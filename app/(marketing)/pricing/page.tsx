05cdca02d03094c89f598c030802611cc63fbf5e
Marc-Olivier Bouchard <mo.bouchard1997@gmail.com>
2023-11-28 15:39:25 -0500

    update pricing

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -92,6 +92,38 @@ export default function PricingPage() {
             <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
                 <div className="grid gap-6">
                     <h3 className="text-xl font-bold sm:text-2xl">
+                        What&apos;s included in the PRO plan
+                    </h3>
+                    <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
+                        <li className="flex items-center">
+                            <Icons.check className="mr-2 h-4 w-4" /> 9 Bots
+                        </li>
+                        <li className="flex items-center">
+                            <Icons.check className="mr-2 h-4 w-4" /> 12 Crawlers
+                        </li>
+                        <li className="flex items-center">
+                            <Icons.check className="mr-2 h-4 w-4" /> 26 Files
+                        </li>
+                        <li className="flex items-center">
+                            <Icons.check className="mr-2 h-4 w-4" /> Premium Support
+                        </li>
+                    </ul>
+                </div>
+                <div className="flex flex-col gap-4 text-center">
+                    <div>
+                        <h4 className="text-7xl font-bold">$27</h4>
+                        <p className="text-sm font-medium text-muted-foreground">
+                            Billed Monthly
+                        </p>
+                    </div>
+                    <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+            <div className="grid w-full items-start gap-10 rounded-lg border p-10 md:grid-cols-[1fr_200px]">
+                <div className="grid gap-6">
+                    <h3 className="text-xl font-bold sm:text-2xl">
                         What&apos;s included in the ENTERPRISE plan
                     </h3>
                     <ul className="grid gap-3 text-sm text-muted-foreground sm:grid-cols-2">
