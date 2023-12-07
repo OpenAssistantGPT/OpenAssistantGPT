0cc96d9177dc2c2630eec0cc2fdd6bcb048cb462
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-07 08:20:52 -0500

    remove useless code

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,18 +4,6 @@ import type { Icon } from "lucide-react"
 
 import { Icons } from "@/components/icons"
 
-import { DefaultUser } from 'next-auth';
-
-declare module 'next-auth' {
-  interface Session {
-    user?: DefaultUser & { id: string; stripeCustomerId: string; isActive: boolean };
-  }
-  interface User extends DefaultUser {
-    stripeCustomerId: string;
-    isActive: boolean;
-  }
-}
-
 export type NavItem = {
   title: string
   href: string
