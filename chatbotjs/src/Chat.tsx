c892b77e49313493fec2fa27f7150faf361d3333
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-10 10:05:58 -0500

    udpate

--- chatbotjs/src/Chat.tsx
+++ chatbotjs/src/Chat.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card } from "@/components/ui/card"
 import { Icons } from "@/components/icons"
+import { siteConfig } from '@/config/site';
 
 interface ChatbotConfig {
   id: number;
