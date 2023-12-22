8fa345d94458d80187f3b76b4f429f4b8e44d7a7
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-22 14:59:09 -0500

    accept all headers

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "*" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
         ]
       }
     ]
