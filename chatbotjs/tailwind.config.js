73538926ef4bc20a2e73bc9bbbc1e0d209c19400
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-22 12:37:40 -0500

    update chatbox look to make it better

--- chatbotjs/tailwind.config.js
+++ chatbotjs/tailwind.config.js
@@ -1,6 +1,7 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  content: ["./src/**/*.{html,css,js,jsx,tsx}"],
   theme: {
     extend: {},
   },
