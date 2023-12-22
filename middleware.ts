ac39322ac1e5b0c96eb345569099622071018337
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-22 14:05:25 -0500

    update middleware to fix 404 bug

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,32 @@ import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
 import { withAuth } from "next-auth/middleware"
 
-
 export default withAuth(
   async function middleware(req) {
 
-    return NextResponse.next()
+    const token = await getToken({ req })
+    const isAuth = !!token
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith("/login")
 
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/dashboard", req.url))
+      }
+
+      return null
+    }
+
+    if (!isAuth) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
   },
   {
     callbacks: {
@@ -21,3 +41,7 @@ export default withAuth(
   }
 )
 
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/login"],
+}
