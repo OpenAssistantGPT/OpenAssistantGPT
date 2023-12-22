3b1dfd56ee0438ecceb1412f34601f39398af11e
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-22 15:22:50 -0500

    update cors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
