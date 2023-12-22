61b92e90401849f2779ac96e5662279a9001592e
Marc-Olivier <mo.bouchard1997@gmail.com>
2023-12-22 15:03:08 -0500

    update headers

--- next.config.js
+++ next.config.js
@@ -3,21 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "*" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-        ]
-      }
-    ]
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig;
