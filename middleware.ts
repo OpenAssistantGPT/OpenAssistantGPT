2deaa63c2f1d89958c5e8c3d6212e6837e73768c
Marc-Olivier <mo.bouchard1997@gmail.com>
2024-01-07 13:45:23 -0500

    update

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
-import { getToken } from "next-auth/jwt"
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
+import { getCurrentUser } from "./lib/session"
 
 export default withAuth(
     async function middleware(req) {
-        const token = await getToken({ req })
-        const isAuth = !!token
+        const user = await getCurrentUser()
+
         const isAuthPage =
             req.nextUrl.pathname.startsWith("/login")
 
         if (isAuthPage) {
-            if (isAuth) {
+            if (user) {
                 return NextResponse.redirect(new URL("/dashboard", req.url))
             }
 
             return null
         }
 
-        if (!isAuth) {
+        if (!user) {
             let from = req.nextUrl.pathname;
             if (req.nextUrl.search) {
                 from += req.nextUrl.search;
