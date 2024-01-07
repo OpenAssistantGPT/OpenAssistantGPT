09d3ebd671d9742bf0222716496c05d2673083bf
Marc-Olivier <mo.bouchard1997@gmail.com>
2024-01-07 13:12:05 -0500

    update chatbotjs

--- chatbotjs/src/index.tsx
+++ chatbotjs/src/index.tsx
@@ -15,7 +15,7 @@ document.body.appendChild(rootDiv);
 const root = createRoot(document.getElementById('chatbot-root'));
 
 root.render(
-    <ShadowRootComponent styleUrl="http://localhost:3000/output.css">
+    <ShadowRootComponent styleUrl="https://www.openassistantgpt.io/chatbot.css">
         <ChatBox />
     </ShadowRootComponent>
 );
