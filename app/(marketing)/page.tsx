26557fcd0ffc7cec335c0802dd11e45a5a4e41b5
Marc-Olivier <mo.bouchard1997@gmail.com>
2024-01-07 20:02:57 -0500

    add twitter link to home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,18 +8,17 @@ import { Icons } from "@/components/icons";
 import { FAQ } from '@/components/faq';
 
 
-
 export default function IndexPage() {
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <Link
-            href={siteConfig.links.productHunt}
+            href={siteConfig.links.twitter}
             className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
             target="_blank"
           >
-            Find us on ProductHunt
+            Find us on Twitter
           </Link>
           <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
             Build your own chatbot with OpenAI Assistant
