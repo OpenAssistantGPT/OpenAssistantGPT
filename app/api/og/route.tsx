c0214494b292921eb7e98aa43566c7b420d0a237
Marc-Olivier <mo.bouchard1997@gmail.com>
2024-01-25 07:21:58 -0500

    update api og

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -116,7 +116,7 @@ export async function GET(req: Request) {
                                     stroke-linejoin="round"
                                 />
                             </svg>
-                            <div tw="flex ml-2">github.com/shadcn/taxonomy</div>
+                            <div tw="flex ml-2">github.com/marcolivierbouch/openassistantgpt</div>
                         </div>
                     </div>
                 </div>
