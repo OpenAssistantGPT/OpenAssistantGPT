406605fb9da51e7ea726bfa2e92f8f74b488fbeb
Marc-Olivier <mo.bouchard1997@gmail.com>
2024-01-31 21:34:35 -0500

    feat: update pricing

--- config/subscriptions.ts
+++ config/subscriptions.ts
@@ -2,60 +2,60 @@ import { SubscriptionPlan } from "@/types"
 
 export const freePlan: SubscriptionPlan = {
     name: "FREE",
-    description: "The FREE plan is limited to 1 chatbot, 1 crawler, 2 files and 20 messages per month.",
+    description: "The FREE plan is limited to 1 chatbot, 1 crawler, 3 files and 500 messages per month.",
     stripePriceId: "",
 
     maxChatbots: 1,
     maxCrawlers: 1,
-    maxFiles: 2,
+    maxFiles: 3,
     unlimitedMessages: false,
-    maxMessagesPerMonth: 10,
+    maxMessagesPerMonth: 500,
 
     price: 0,
 }
 
 export const hobbyPlan: SubscriptionPlan = {
     name: "HOBBY",
-    description: "The HOBBY plan is limited 1 chatbot, 1 crawler and 2 files and unlimited messages.",
+    description: "The HOBBY plan is limited 3 chatbot, 3 crawler and 9 files and unlimited messages.",
     stripePriceId: process.env.NEXT_PUBLIC_STRIPE_HOBBY_PRICE_ID || "",
 
-    maxChatbots: 1,
-    maxCrawlers: 1,
-    maxFiles: 2,
-    unlimitedMessages: false,
-    maxMessagesPerMonth: 100,
+    maxChatbots: 3,
+    maxCrawlers: 3,
+    maxFiles: 9,
+    unlimitedMessages: true,
+    maxMessagesPerMonth: undefined,
 
     price: 3,
 }
 
 export const basicPlan: SubscriptionPlan = {
     name: "BASIC",
-    description: "The BASIC plan has 3 chatbots, 3 crawlers and 6 files and unlimited messages.",
+    description: "The BASIC plan has 9 chatbots, 9 crawlers and 27 files and unlimited messages.",
     stripePriceId: process.env.NEXT_PUBLIC_STRIPE_BASIC_PRICE_ID || "",
 
-    maxChatbots: 3,
-    maxCrawlers: 3,
-    maxFiles: 6,
+    maxChatbots: 9,
+    maxCrawlers: 9,
+    maxFiles: 27,
     unlimitedMessages: true,
     maxMessagesPerMonth: undefined,
 
     premiumSupport: true,
 
-    price: 8,
+    price: 9,
 }
 
 export const proPlan: SubscriptionPlan = {
     name: "PRO",
-    description: "The PRO plan has 12 chatbots, 12 crawlers and 24 files and unlimited messages.",
+    description: "The PRO plan has 27 chatbots, 27 crawlers and 81 files and unlimited messages.",
     stripePriceId: process.env.NEXT_PUBLIC_STRIPE_PRO_PRICE_ID || "",
 
-    maxChatbots: 12,
-    maxCrawlers: 12,
-    maxFiles: 24,
+    maxChatbots: 27,
+    maxCrawlers: 27,
+    maxFiles: 81,
     unlimitedMessages: true,
     maxMessagesPerMonth: undefined,
 
     premiumSupport: true,
 
-    price: 30,
+    price: 27,
 }
