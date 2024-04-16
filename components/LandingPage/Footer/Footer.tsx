279c21992b6b3867e79f46dfa4557042caa91a78
tanjinahakim <tanjinahakim44@gmail.com>
2024-04-16 07:41:25 +0600

    fixed footer copyright text position

--- components/LandingPage/Footer/Footer.tsx
+++ components/LandingPage/Footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const FooterColumn: React.FC<{ title: string; items: string[] }> = ({ title, items }) => {
     return (
       <div className="flex flex-col">
-        <h4 className="text-md font-bold mb-4">{title}</h4>
+        <h4 className="font-bold mb-4">{title}</h4>
         <ul className="text-[#6460b6]">
           {items.map((item, index) => (
             <li key={index} className="mb-2 hover:underline">
@@ -37,7 +37,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="bg-[#1E1B2C] text-white py-12">
+    <footer className="bg-[#1E1B2C] text-white py-8">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
         {/* Render each column using FooterColumn component */}
         {columns.map((column, index) => (
@@ -45,7 +45,7 @@ const Footer = () => {
         ))}
       </div>
       {/* Copyright and Support */}
-      <div className="text-center mt-8 text-[#6460b6]">
+      <div className="w-[80%] flex justify-end mt-8 text-[#6460b6]">
         <p className="text-sm">
           © {new Date().getFullYear()} 24 Ventures Ltd | Support:{" "}
           <a href="mailto:support@aminos.ai" className="underline">support@aminos.ai</a>
