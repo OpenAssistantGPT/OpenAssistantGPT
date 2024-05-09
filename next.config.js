00f0393831d34abfc3adcaa63551992f2ac4fc94
Marc-Olivier Bouchard <mo.bouchard1997@gmail.com>
2024-05-09 08:46:14 -0400

    feat: disable suspense

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
