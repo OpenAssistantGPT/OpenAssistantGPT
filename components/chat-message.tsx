62c09dfda2887d2375858fa1398bc87eb3229a77
marcolivierbouch <mo.bouchard1997@gmail.com>
2024-06-18 07:28:18 -0400

    feat: update dir

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -23,33 +23,20 @@ export interface ChatMessageProps {
 const getDirection = (isRTL: boolean) => isRTL ? 'rtl' : 'ltr';
 
 export function ChatMessage({ message, children, chatbot, isFirst, ...props }: ChatMessageProps) {
-    const isRTL = chatbot.rightToLeftLanguage;
-    const direction = getDirection(isRTL);
-
     return (
         <>
             {
                 message.role === 'user' ? (
                     <div
-                        className={cn('group relative mb-4 flex justify-end items-end', { 'pr-10': isRTL, 'pl-10': !isRTL })}
-                        dir={direction}
+                        className={cn('pl-10 group relative mb-4 flex justify-end items-end')}
                         {...props}
                     >
                         <p
-                            style={{
-                                color: chatbot.userReplyTextColor,
-                                background: chatbot.userReplyBackgroundColor
-                            }}
-                            className={`p-2 rounded-lg ${isRTL ? 'ml-4' : 'mr-4'}`}
+                            style={{ color: chatbot.userReplyTextColor, background: chatbot.userReplyBackgroundColor }}
+                            className="p-2 rounded-lg mr-4"
+                            dir={getDirection(chatbot.rightToLeftLanguage)} // Set text direction
                         >
-                            <svg
-                                fill={chatbot.userReplyBackgroundColor}
-                                className={`absolute bottom-0 ${isRTL ? 'left-11' : 'right-11'}`}
-                                height="14"
-                                width="13"
-                            >
-                                <path d="M6 .246c-.175 5.992-1.539 8.89-5.5 13.5 6.117.073 9.128-.306 12.5-3L6 .246Z"></path>
-                            </svg>
+                            <svg fill={chatbot.userReplyBackgroundColor} className="absolute bottom-[0px] right-11" height="14" width="13"><path d="M6 .246c-.175 5.992-1.539 8.89-5.5 13.5 6.117.073 9.128-.306 12.5-3L6 .246Z"></path></svg>
                             {message.content}
                         </p>
                         <div
@@ -63,19 +50,10 @@ export function ChatMessage({ message, children, chatbot, isFirst, ...props }: C
                     </div>
                 ) : (
                     <div
-                        className={cn('group relative mb-4 flex items-start', { 'pl-10': isRTL, 'pr-10': !isRTL })}
-                        dir={direction}
+                        className={cn('pr-10 group relative mb-4 flex items-start ')}
                         {...props}
                     >
-                        {chatbot.chatbotLogoURL ? (
-                            <Image
-                                className='size-8'
-                                width={50}
-                                height={50}
-                                src={chatbot.chatbotLogoURL}
-                                alt="chatbot logo"
-                            />
-                        ) : (
+                        {chatbot.chatbotLogoURL ? <Image className='size-8' width={50} height={50} src={chatbot.chatbotLogoURL} alt="chatbot logo" /> :
                             <div
                                 className={cn(
                                     'flex size-8 shrink-0 select-none items-center justify-center rounded-md border shadow',
@@ -84,11 +62,9 @@ export function ChatMessage({ message, children, chatbot, isFirst, ...props }: C
                             >
                                 <Icons.bot />
                             </div>
-                        )}
-                        <div className={`flex-1 px-1 ${isRTL ? 'mr-4' : 'ml-4'}`}>
-                            {message.content === "loading" ? (
-                                <Icons.loading className="animate-spin" />
-                            ) : (
+                        }
+                        <div className="flex-1 px-1 ml-4">
+                            {message.content == "loading" ? <Icons.loading className="animate-spin" /> :
                                 <>
                                     <MemoizedReactMarkdown
                                         className="w-full prose break-words prose-p:leading-relaxed prose-pre:p-0"
@@ -102,7 +78,7 @@ export function ChatMessage({ message, children, chatbot, isFirst, ...props }: C
                                                 )
                                             },
                                             p({ children }) {
-                                                return <p className="mb-2 last:mb-0">{children}</p>
+                                                return <p className="mb-2 last:mb-0" dir={getDirection(chatbot.rightToLeftLanguage)}>{children}</p>
                                             },
                                             code({ node, className, children, ...props }) {
                                                 const match = /language-(\w+)/.exec(className || '')
@@ -141,7 +117,7 @@ export function ChatMessage({ message, children, chatbot, isFirst, ...props }: C
                                         <div className='size-3'></div>
                                     }
                                 </>
-                            )}
+                            }
                         </div>
                     </div>
                 )
